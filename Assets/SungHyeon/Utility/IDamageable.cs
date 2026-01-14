799bff2bcc6972f32a9454e70de24c9adb092557
ckdtjdeo12-debug <ckdtjdeo12@gmail.com>
2026-01-14 16:52:08 +0900

    IDamageable

--- Assets/SungHyeon/Utility/IDamageable.cs
+++ Assets/SungHyeon/Utility/IDamageable.cs
@@ -4,9 +4,8 @@ namespace TeamProject2
 {
     /// <summary>
     /// 데미지를 관리하는 클래스
-    /// Health를 관리하는 클래스
     /// </summary>
-    public interface Damageable 
+    public interface IDamageable 
     {
         public void TakeDamage(float damage);
     }
